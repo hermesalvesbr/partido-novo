d2f4ad75b60a7189a55deb2abb67871ff5157bbe
Hermes Alves <hermes@softagon.com.br>
2025-12-30 22:53:22 -0300

    feat: add Google Search Console verification meta tag

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+// Google Search Console verification
+useHead({
+  meta: [
+    { name: 'google-site-verification', content: 'WyegARB7I2v1AvlTzR0sheLKNpZ94hhOpFNJO4SWuCo' },
+  ],
+})
+</script>
+
 <template>
   <div>
     <NuxtLoadingIndicator color="#FF9800" :height="4" />
